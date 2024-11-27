9e6baa182dcaa0914eec4edf5f7c0a2e74cfae1b
REIGaruki <gyakujo@mail.ru>
2024-11-27 14:16:08 +0300

    Stream solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         String[] stringArray = new String[]{"_first_", "_second_", "_third_", "_second_", "_first_", "_fourth_", "_third_"};
         System.out.println(taskSet(stringArray));
+        System.out.println(taskStream(stringArray));
     }
 
     public static String taskSet(String[] stringArray) {
@@ -18,5 +20,9 @@ public class Main {
             result.append(string);
         }
         return result.toString();
+    }
+
+    public static String taskStream(String[] stringArray) {
+        return Arrays.stream(stringArray).distinct().collect(Collectors.joining());
     }
 }
