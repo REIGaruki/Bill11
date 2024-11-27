b62386a732f99567f8daab3a70cc9fd7cf9aba46
REIGaruki <gyakujo@mail.ru>
2024-11-27 14:14:27 +0300

    LinkedHashSet solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        String[] stringArray = new String[]{"_first_", "_second_", "_third_", "_second_", "_first_", "_fourth_", "_third_"};
+        System.out.println(taskSet(stringArray));
+    }
 
+    public static String taskSet(String[] stringArray) {
+        Set<String> stringSet = new LinkedHashSet<String>();
+        for (String string : stringArray) {
+            stringSet.add(string);
+        }
+        StringBuilder result = new StringBuilder();
+        for (String string : stringSet) {
+            result.append(string);
+        }
+        return result.toString();
     }
 }
